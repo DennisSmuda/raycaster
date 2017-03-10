8a3b538b72e8d04f4d164b9b892fe49186d07730
Dennis Smuda <dennissmuda@msn.com>
2017-03-10 07:33:21 +0100

    Player..

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import map from './map'
 import Raycaster from './raycaster'
+import Player from './player'
 
 let mapWidth = 0;
 let mapHeight = 0;
@@ -7,47 +8,96 @@ const miniMapScale = 8;
 
 const bgColor = '56, 42, 36';
 
-(function() {
 
-  let canvas = document.getElementById('canvas');
-  let miniMap = document.getElementById('minimap');
-  let raycaster = new Raycaster();
+let canvas           = document.getElementById('canvas');
+let miniMap          = document.getElementById('minimap');
+let raycaster        = new Raycaster();
 
-  mapWidth = map[0].length;
-	mapHeight = map.length;
+mapWidth             = map[0].length;
+mapHeight            = map.length;
 
-  miniMap.width = mapWidth * miniMapScale;
-  miniMap.height = mapHeight * miniMapScale;
-  miniMap.style.width = (mapWidth * miniMapScale) + "px";
-  miniMap.style.height = (mapHeight * miniMapScale) + "px";
+miniMap.width        = mapWidth * miniMapScale;
+miniMap.height       = mapHeight * miniMapScale;
+miniMap.style.width  = (mapWidth * miniMapScale) + "px";
+miniMap.style.height = (mapHeight * miniMapScale) + "px";
 
 
+
+// Initialize
+player = new Player();
+bindKeys();
+
+
+loop();
+
+
+
+
+function loop() {
   drawMiniMap();
+  move();
 
 
-  /**
-   * Draw Top-Down 2d View
-   *
-   */
-  function drawMiniMap() {
-    let ctx = miniMap.getContext("2d");
+  window.requestAnimationFrame(loop);
 
-    for (let y = 0; y < mapHeight; y++) {
-      for (let x = 0; x < mapWidth; x++) {
-        let wall = map[y][x];
+}
 
-        if (wall > 0) {
-          ctx.fillStyle = "rgb(124, 67, 43)";
-          ctx.fillRect(
-            x * miniMapScale,
-            y * miniMapScale,
-            miniMapScale, miniMapScale
-          );
-        }
+
+/**
+ * Draw Top-Down 2d View
+ *
+ */
+function drawMiniMap() {
+  let ctx = miniMap.getContext("2d");
+
+  for (let y = 0; y < mapHeight; y++) {
+    for (let x = 0; x < mapWidth; x++) {
+      let wall = map[y][x];
+
+      if (wall > 0) {
+        ctx.fillStyle = "rgb(124, 67, 43)";
+        ctx.fillRect(
+          x * miniMapScale,
+          y * miniMapScale,
+          miniMapScale, miniMapScale
+        );
       }
     }
   }
-  console.log(map);
+}
 
-})();
+
+
+function bindKeys() {
+  document.onkeydown = function(e) {
+      e = e ||  window.event;
+
+      switch (e.keyCode) {
+        case 38: // Up
+          player.speed = 1;
+          break;
+
+        case 40: // Down
+          player.speed = -1;
+          break;
+
+        case 37: // Left
+          player.dir = -1;
+          break;
+
+        case 39: // Right
+          player.dir = -1;
+          break;
+    }
+  }
+
+  document.onkeyup = function(e) {
+
+  }
+}
+
+function move() {
+  let moveStep = player.speed * player.moveSpeed;
+  player.rot += player.dir * player.rotSpeed * Math.PI / 180;
+}
 
---
+++ src/player.js
@@ -0,0 +1,15 @@
+class Player {
+
+  constructor() {
+    this.x = 1;
+    this.y = 1;
+    this.dir = 0;
+    this.rot = 0; // the current angle of rotation
+    this.speed = 0; // is the playing moving forward (speed = 1) or backwards (speed = -1).
+    this.moveSpeed = 0.18; // how far (in map units) does the player move each step/update
+    this.rotSpeed = 6; // how much does the player rotate each step/update (in degrees)
+  }
+}
+
+export default Player
+
